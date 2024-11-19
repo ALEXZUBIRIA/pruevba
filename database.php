7524e552b08e4bbe9c385bb3066d6593614779ff
ALEXZUBIRIA <100256176+ALEXZUBIRIA@users.noreply.github.com>
2024-11-18 22:17:13 -0600

    Update database.php

--- database.php
+++ database.php
@@ -1,13 +1,15 @@
 <?php
-$host = getenv('DB_HOST'); // 'localhost' para entorno local
+$host = getenv('DB_HOST');
 $dbname = getenv('DB_NAME');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
+$port = getenv('PORT'); // Añadir esto si no está ya
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa al puerto $port";
 } catch (PDOException $e) {
     die("Error en la conexión: " . $e->getMessage());
 }
-
+?>
